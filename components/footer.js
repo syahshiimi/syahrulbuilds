c046536193009fa4b1f74ecef1be67257b589855
syahshiimi <syahrulanuar@Syahruls-MacBook-Air.local>
2022-05-22 13:42:46 +0800

    added links to footer

--- components/footer.js
+++ components/footer.js
@@ -3,9 +3,9 @@ import { Flex, Text } from "@chakra-ui/react";
 export default function Footer() {
   return (
     <Flex as="footer" justify="center" color={"white"}>
-      <Text mr="2vh">github</Text>
-      <Text mr="2vh">instagram</Text>
-      <Text mr="2vh">linkedin</Text>
+      <Text mr="2vh"><a href="https://github.com/syahshiimi">github</a></Text>
+      <Text mr="2vh"><a href="https://www.instagram.com/syahshiimi/">instagram</a></Text>
+      <Text mr="2vh"><a href="https://sg.linkedin.com/in/syahrul-anuar-212274102?original_referer=https%3A%2F%2Fsg.linkedin.com%2F">linkedin</a></Text>
     </Flex>
   );
 }
