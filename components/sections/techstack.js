8bf767baaea525e4cd06209e8e8ef5bf397a2a69
syahshiimi <syahrlanuar95@gmail.com>
2022-05-26 01:31:51 +0800

    footer margin updates

--- components/sections/techstack.js
+++ components/sections/techstack.js
@@ -1,4 +1,4 @@
-import { Stack, Box, Heading, Tag, Text, Wrap } from "@chakra-ui/react";
+import { Stack, Box, Container, Heading, Tag, Text, Wrap } from "@chakra-ui/react";
 
 // Data
 import { FrameworksLang } from "../../data/languages";
@@ -10,21 +10,22 @@ import Apps from "../../components/techstacks/apps";
 
 export default function Techstack() {
     return (
-
         <Box
             className="c-homecontent__techstack"
-            mt={8} color={'gray.50'}>
+            mt={28} color={'gray.50'}>
             <Heading size="md" lineHeight="6" color="purple.100" mb="21px">
                 My Tech Stack
             </Heading>
-            <Text size="sm" lineHeight="5" mb={20}>
-                Apart from my tech stack, I have also acquired skills that were
-                related to arts and design such as the{" "}
-                <Text as="span" color="red.400">
-                    Adobe Creative Cloud Suite
-                </Text>{" "}
-                and proficiency in graphics and media related content.
-            </Text>
+            <Container>
+                <Text fontSize="sm" lineHeight="5" mb={16} fontWeight='medium'>
+                    Apart from my tech stack, I have also acquired skills that were
+                    related to arts and design such as the{" "}
+                    <Text as="span" color="red.400">
+                        Adobe Creative Cloud Suite
+                    </Text>{" "}
+                    and proficiency in graphics and media related content.
+                </Text>
+            </Container>
             <Stack
                 className='c-homecontent__languages'
                 spacing={'18px'}
