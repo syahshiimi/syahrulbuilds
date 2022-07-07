2676b7956a71b761ed9aea629133970b369854d0
syahshiimi <syahrlanuar95@gmail.com>
2022-07-08 01:27:58 +0800

    started on pastworks mdx

--- components/readmorebutton.js
+++ components/readmorebutton.js
@@ -1,11 +1,16 @@
-import { Button } from '@chakra-ui/react';
+import { Button } from "@chakra-ui/react";
 
 const ReadMore = () => {
-    return (
-        <Button className="c-readmore__button" variant={"link"} size={'sm'} alignItems='right'>
-            Read More
-        </Button>
-    )
-}
+  return (
+    <Button
+      className="c-readmore__button"
+      variant={"link"}
+      size={"sm"}
+      alignItems="right"
+    >
+      Read More
+    </Button>
+  );
+};
 
 export default ReadMore;
